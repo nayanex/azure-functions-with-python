3e9f5c7e7d6ddc5bda0be946faa518388e19f376
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-06-03 22:05:46 +0200

    getNote endpoint

--- getNote/__init__.py
+++ getNote/__init__.py
@@ -4,6 +4,9 @@ import os
 
 import azure.functions as func
 from bson.json_util import dumps
+
+# importing ObjectId from bson library
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 from config import Config
@@ -12,18 +15,28 @@ from config import Config
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Attempting to connect to MongoDB database :)")
 
-    try:
-        client = MongoClient(Config.connection_uri)
-        db = client[Config.MONGO_DB_NAME]
-        
-        notes_collection = db["notes"]
+    id = req.params.get("id")
+    if not id:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            id = req_body.get("id")
 
-        result = notes_collection.find({})
-        result = dumps(result)
+    if id:
+        try:
+            client = MongoClient(Config.connection_uri)
+            db = client[Config.MONGO_DB_NAME]
+            notes_collection = db["notes"]
 
-        return func.HttpResponse(
-            result, mimetype="application.json", charset="utf-8", status_code=200
-        )
+            objInstance = ObjectId(id)
+            result = notes_collection.find_one({"_id": objInstance})
+            result = dumps(result)
 
-    except ConnectionError:
-        return func.HttpRequest("Bad Request", status_code=400)
+            return func.HttpResponse(
+                result, mimetype="application.json", charset="utf-8", status_code=200
+            )
+
+        except ConnectionError:
+            return func.HttpRequest("Bad Request", status_code=400)
